00537997df15e1e0cbc408d0bbf26dbe6c024843
Aditya-shou <adityashou99@gmail.com>
2023-11-21 18:40:36 +0530

    Adding the project

---
+++ src/main/java/com/labda/my_lab_da/LoginMatcher.java
@@ -0,0 +1,10 @@
+package com.labda.my_lab_da;
+
+
+
+public class LoginMatcher
+{
+	public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/com/labda/my_lab_da/LoginMatcherTest.java
@@ -0,0 +1,12 @@
+package com.labda.my_lab_da;
+
+import org.junit.Test;
+
+
+public class LoginMatcherTest {
+	@Test
+    public void testHelloWorld() {
+		LoginMatcher helloWorld = new LoginMatcher();
+        
+    }
+}
